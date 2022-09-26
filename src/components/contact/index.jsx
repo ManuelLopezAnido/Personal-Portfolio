e4c712bb4847ac4c271c14dc2cf329196c1aae55
Manuel Lopez Anido <manuellopezanido@gmail.com>
2022-09-26 19:47:56 -0300

    portafolio almost done

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react'
 import styles from './contact.module.scss'
-const contact = ()=>{
+const Contact = ()=>{
+  const [messege, setMessege] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setMessege(true)
+  }
   return (
     <div className={styles.contact} id={'contact'}>
-      Contact
+      <div className={styles.left}>
+        <img src='assets/shake.svg' alt=''/>
+      </div>
+      <div className={styles.right}>
+        <h2>
+          Contact.
+        </h2>
+        <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='Email'/>
+            <textarea placeholder='Message'></textarea>
+            <button type='submit'>Send</button>
+            {messege && <span> Thanks, I'll reply as soon as posible</span>}            
+        </form>
+      </div>
     </div>
   )
 }
-export default contact
+export default Contact
--- src/components/testimonials/index.jsx
+++ src/components/testimonials/index.jsx
@@ -1,8 +1,55 @@
 import styles from './testimonials.module.scss'
+
 const testimonials = ()=>{
+  const data = [
+    {
+      name: 'Heraldo Gerber',
+      position: 'Liliana IT Manager',
+      testimonial: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.' +
+                    '  Numquam odio animi debitis repellendus '+
+                    'cumque officia provident quo accusamus.',
+      image: 'heraldo.jpg',
+      feature: false
+    },
+    {
+      feature: false
+    },
+    {
+      feature: false
+    },
+    {
+      
+    }
+  ]
   return (
     <div className={styles.testimonials} id={'testimonials'}>
-      Testimonials
+      <h1> Testimonials </h1>
+      <div className={styles.container}>
+        {
+          data.map((person)=>{
+            return(
+              <div className={person.feature ? `${styles.card} ${styles.featured} ` : styles.card }>
+                <div className={styles.top}>
+                  <img src={'assets/right-arrow.png'} alt={''} className={styles.left}/>
+                  <img src={'assets/' + person.image } alt={''} className={styles.user}/>
+                  <img src={'assets/linkedin.png'} alt={''} className={styles.right}/>
+                </div>
+                <div className={styles.center}>
+                  {person.testimonial}
+                </div>
+                <div className={styles.bottom}>
+                  <h3>
+                    {person.name}
+                  </h3>
+                  <h4>
+                    {person.position} 
+                  </h4>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
